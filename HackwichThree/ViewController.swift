0f05b6f26933a4ef8f36c66c0fa04cbc1b8f2ceb
Cliff Bailey <69611676+phazonxiii@users.noreply.github.com>
2022-02-10 10:07:05 -1000

    Initial Commit

---
+++ HackwichThree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackwichThree
+//
+//  Created by Clifford Bailey on 2202/10/.
+//  Copyright © 2022 Cliff Bailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HackwichThreeTests/HackwichThreeTests.swift
@@ -0,0 +1,34 @@
+//
+//  HackwichThreeTests.swift
+//  HackwichThreeTests
+//
+//  Created by Clifford Bailey on 2202/10/.
+//  Copyright © 2022 Cliff Bailey. All rights reserved.
+//
+
+import XCTest
+@testable import HackwichThree
+
+class HackwichThreeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
